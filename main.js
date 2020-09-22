e2a8e606b74a857001095797bb24922a52dd949f
Kachi <nzebenflorence@gmail.com>
2020-09-22 16:57:31 +0100

    color fix

--- main.js
+++
@@ -1,8 +0,0 @@
- myFunction = ()=> {
-    let navLink = document.querySelector("nav-links");
-    if (navLink.style.display === "block") {
-      navLink.style.display = "none";
-    } else {
-      navLink.style.display = "block";
-    }
-  }
